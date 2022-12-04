800e508e4d6a0c43a18b08f26ca3c39e97aab1ee
Exxen <71314753+RevertSucks@users.noreply.github.com>
2022-12-04 05:41:17 -0600

    Create GetItems.lua

---
+++ GetItems.lua
@@ -0,0 +1,56 @@
+local plr = game.Players.LocalPlayer
+local guis = plr.PlayerGui
+--MUST RUN CMDS COMMAND OR ELSE THIS WONT WORK!
+local cmdlister = guis["CmdLister"]
+
+local toolfolder = cmdlister["ToolFolder"]
+local armorfolder = cmdlister["ArmorFolder"]
+
+_G.guns = {}
+_G.attachments = {}
+_G.ammo = {}
+_G.other = {}
+_G.gp_tool = {}
+
+_G.armor = {}
+_G.gp_armor = {}
+
+ for i,v in pairs(toolfolder:GetDescendants()) do
+    if not v:IsA("Folder") then
+        
+        if v.Parent.Name == "Guns" then
+            table.insert(_G.guns,v.Name)
+        end
+
+        if v.Parent.Name == "Attachments" then
+            table.insert(_G.attachments,v.Name)
+        end
+
+        if v.Parent.Name == "Ammo" then
+            table.insert(_G.ammo,v.Name)
+        end
+
+        if v.Parent.Name == "Other" then
+            table.insert(_G.other,v.Name)
+        end
+
+        if v.Parent.Name == "Gamepass" then
+            table.insert(_G.gp_tool,v.Name)
+        end
+
+    end
+ end
+
+ for i,v in pairs(armorfolder:GetDescendants()) do
+    if not v:IsA("Folder") then
+        
+        if v.Parent.Name == "Armor" then
+            table.insert(_G.armor,v.Name)
+        end
+
+        if v.Parent.Name == "Gamepass" then
+            table.insert(_G.gp_armor,v.Name)
+        end
+
+    end
+ end
